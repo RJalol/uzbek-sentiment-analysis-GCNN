6c6d7df7e5fb7216102326081d4dcad00416ef35
RJalol <109087737+RJalol@users.noreply.github.com>
2025-08-19 11:11:10 +0500

    Update cnn_gate_aspect_model.py

--- model_files/cnn_gate_aspect_model.py
+++ model_files/cnn_gate_aspect_model.py
@@ -24,23 +24,37 @@ class CNN_Gate_Aspect_Text(nn.Module):
 
         self.convs1 = nn.ModuleList([nn.Conv1d(D, Co, K) for K in Ks])
         self.convs2 = nn.ModuleList([nn.Conv1d(D, Co, K) for K in Ks])
+        self.convs3 = nn.ModuleList([nn.Conv1d(D, Co, K, padding=K-2) for K in [3]])
+
+
+        # self.convs3 = nn.Conv1d(D, 300, 3, padding=1), smaller is better
+        self.dropout = nn.Dropout(0.2)
 
         self.fc1 = nn.Linear(len(Ks)*Co, C)
-        self.fc_aspect = nn.Linear(args.aspect_embed_dim, Co)
+        self.fc_aspect = nn.Linear(100, Co)
+
 
     def forward(self, feature, aspect):
         feature = self.embed(feature)  # (N, L, D)
         aspect_v = self.aspect_embed(aspect)  # (N, L', D)
-        aspect_v = aspect_v.sum(1) / aspect_v.size(1)
+        aa = [F.relu(conv(aspect_v.transpose(1, 2))) for conv in self.convs3]  # [(N,Co,L), ...]*len(Ks)
+        aa = [F.max_pool1d(a, a.size(2)).squeeze(2) for a in aa]
+        aspect_v = torch.cat(aa, 1)
+        # aa = F.tanhshrink(self.convs3(aspect_v.transpose(1, 2)))  # [(N,Co,L), ...]*len(Ks)
+        # aa = F.max_pool1d(aa, aa.size(2)).squeeze(2)
+        # aspect_v = aa
+        # smaller is better
 
         x = [F.tanh(conv(feature.transpose(1, 2))) for conv in self.convs1]  # [(N,Co,L), ...]*len(Ks)
         y = [F.relu(conv(feature.transpose(1, 2)) + self.fc_aspect(aspect_v).unsqueeze(2)) for conv in self.convs2]
         x = [i*j for i, j in zip(x, y)]
 
         # pooling method
-        x0 = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N,Co), ...]*len(Ks)
-        x0 = [i.view(i.size(0), -1) for i in x0]
+        x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N,Co), ...]*len(Ks)
+        # x = [F.adaptive_max_pool1d(i, 2) for i in x]
+        # x = [i.view(i.size(0), -1) for i in x]
 
-        x0 = torch.cat(x0, 1)
-        logit = self.fc1(x0)  # (N,C)
+        x = torch.cat(x, 1)
+        x = self.dropout(x)  # (N,len(Ks)*Co)
+        logit = self.fc1(x)  # (N,C)
         return logit, x, y
